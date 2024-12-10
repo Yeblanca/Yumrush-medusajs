c126349b90e435375b5673e3a698ac133d2a8252
Yeblanca <yp@yeblanca.com>
2024-12-10 00:53:58 -0600

    Update index.tsx

--- medusajs-storefront/src/modules/layout/components/cart-dropdown/index.tsx
+++ medusajs-storefront/src/modules/layout/components/cart-dropdown/index.tsx
@@ -94,7 +94,7 @@ const CartDropdown = ({
           </LocalizedClientLink>
         </Popover.Button> */}
         <Transition
-          show={true}
+          show={cartDropdownOpen}
           as={Fragment}
           enter="transition ease-out duration-200"
           enterFrom="opacity-0 translate-y-1"
